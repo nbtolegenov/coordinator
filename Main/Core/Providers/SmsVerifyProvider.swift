8b9ba1bf4850fc7295ba2f4d4e72ebab49154cf4
Nurlan Tolegenov <nbtolegenov@gmail.com>
2020-08-21 12:29:40 +0600

    feat: add sms verify provider

---
+++ Main/Core/Providers/SmsVerifyProvider.swift
@@ -0,0 +1,24 @@
+//
+//  SmsVerifyProvider.swift
+//  Coordinator
+//
+//  Created by Nurlan Tolegenov on 8/21/20.
+//  Copyright © 2020 Nurlan Tolegenov. All rights reserved.
+//
+
+import Foundation
+import Promises
+
+protocol SmsVerifyProvider {
+    func verifySms(phoneNumber: String, smsId: String, code: String) -> Promise<VerifySmsResponse>
+}
+
+extension NetworkProvider: SmsVerifyProvider {
+    func verifySms(phoneNumber: String, smsId: String, code: String) -> Promise<VerifySmsResponse> {
+        Promise(VerifySmsResponse(token: "Token"))
+    }
+}
+
+struct VerifySmsResponse: Decodable {
+    let token: String
+}
